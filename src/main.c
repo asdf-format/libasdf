63137baab7b4a29803b373b0d0a3114955965e60
E. Madison Bray <676149+embray@users.noreply.github.com>
2026-05-14 17:28:14 +0200

    feat: new `asdf dd` command-line interface; very useful for testing too

--- src/main.c
+++ src/main.c
@@ -4,11 +4,13 @@
 
 
 #include <argp.h>
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "core/ndarray.h"
 #include "event.h"
 #include "file.h"
 #include "info.h"
@@ -29,6 +31,7 @@ const char *argp_program_bug_address = PACKAGE_BUGREPORT;
 // but my system's clang-format doesn't have this setting yet.
 typedef enum {
     ASDF_SUBCMD_NONE = 0,
+    ASDF_SUBCMD_DD,
     ASDF_SUBCMD_INFO,
     ASDF_SUBCMD_EVENTS,
     ASDF_SUBCMD_VERIFY_CHECKSUMS
@@ -66,6 +69,228 @@ static char doc[] = "asdf -- Commandline utilities for managing ASDF files."
 
 
 // info subcommand
+static char dd_doc[] = "Dump data from an ASDF binary block";
+static char dd_args_doc[] = "INPUT [OUTPUT|-]";
+
+
+static struct argp_option dd_options[] = {
+    {"block", 'b', "N", 0, "Block index from which to extract", 0},
+    {"ndarray", 'n', "PATH", 0, "Tree path of an ndarray from which to extract", 0},
+    {"raw",
+     'r',
+     0,
+     0,
+     "Dump raw compressed data if compressed (no effect on non-compressed data)",
+     0},
+    {"chunk-size", 'c', "N", 0, "Read/write chunk size", 0},
+    {"help", 'h', 0, 0, "Give this help list", 0},
+    {"usage", OPT_USAGE_KEY, 0, 0, "Give a short usage message", 0},
+    {0}};
+
+
+struct dd_args {
+    const char *input;
+    const char *output;
+    size_t block_index;
+    const char *ndarray_path;
+    bool has_block_index;
+    size_t chunk_size;
+    bool raw;
+};
+
+
+#define MAX_UINT_DIGITS 10
+
+
+// NOLINTNEXTLINE(readability-non-const-parameter)
+static error_t parse_dd_opt(int key, char *arg, struct argp_state *state) {
+    struct dd_args *args = state->input;
+
+    switch (key) {
+    case 'b': {
+        char *end = NULL;
+        errno = 0;
+        unsigned long value = strtoul(arg, &end, MAX_UINT_DIGITS);
+
+        if (errno != 0 || *end != '\0')
+            argp_error(state, "Invalid block index: %s", arg);
+
+        args->block_index = value;
+        args->has_block_index = true;
+        break;
+    }
+    case 'n':
+        args->ndarray_path = arg;
+        break;
+    case 'c': {
+        char *end = NULL;
+        errno = 0;
+        unsigned long value = strtoul(arg, &end, MAX_UINT_DIGITS);
+
+        if (errno != 0 || *end != '\0')
+            argp_error(state, "Invalid chunk size: %s", arg);
+
+        args->chunk_size = value;
+        break;
+    }
+    case 'r':
+        args->raw = true;
+        break;
+    case 'h':
+        argp_state_help(state, stdout, ARGP_HELP_STD_HELP);
+        break;
+    case OPT_USAGE_KEY:
+        argp_state_help(state, stdout, ARGP_HELP_SHORT_USAGE | ARGP_HELP_EXIT_OK);
+        break;
+    case ARGP_KEY_ARG:
+        if (!args->input)
+            args->input = arg;
+        else if (!args->output)
+            args->output = arg;
+        else
+            argp_error(
+                state,
+                "Too many arguments for 'dd'. Only INPUT and OUTPUT "
+                "are expected.");
+        break;
+    case ARGP_KEY_NO_ARGS:
+        argp_error(state, "'dd' requires INPUT and OUTPUT arguments.");
+        break;
+    case ARGP_KEY_END:
+        if (!(args->has_block_index || args->ndarray_path) ||
+            (args->has_block_index && args->ndarray_path))
+            argp_error(state, "'dd' requires only one of --block or --ndarray");
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+
+static struct argp dd_argp = {dd_options, parse_dd_opt, dd_args_doc, dd_doc, 0, 0, 0};
+
+
+static int dd_run(struct dd_args args) {
+    asdf_file_t *file = asdf_open(args.input, "r");
+    asdf_block_t *block = NULL;
+    asdf_ndarray_t *ndarray = NULL;
+    size_t size = 0;
+    const void *data = NULL;
+    FILE *fp = NULL;
+    bool close = false;
+    size_t written = 0;
+    int status = EXIT_FAILURE;
+
+    if (!file) {
+        fprintf(stderr, "error: %s\n", asdf_error(NULL));
+        return EXIT_FAILURE;
+    }
+
+    if (args.has_block_index) {
+        size_t block_count = asdf_block_count(file);
+        if (args.block_index + 1 > block_count) {
+            fprintf(
+                stderr,
+                "error: block index %zu not found (file contains %zu blocks)\n",
+                args.block_index,
+                block_count);
+            goto cleanup;
+        }
+
+        block = asdf_block_open(file, args.block_index);
+
+        if (args.raw)
+            data = asdf_block_data_raw(block, &size);
+        else
+            data = asdf_block_data(block, &size);
+    } else if (args.ndarray_path) {
+        asdf_value_err_t err = asdf_get_ndarray(file, args.ndarray_path, &ndarray);
+
+        if (err == ASDF_VALUE_ERR_NOT_FOUND || err == ASDF_VALUE_ERR_TYPE_MISMATCH) {
+            fprintf(stderr, "error: no ndarray found at path '%s'\n", args.ndarray_path);
+            goto cleanup;
+        }
+
+        if (err != ASDF_VALUE_OK) {
+            fprintf(
+                stderr,
+                "error: failed to read ndarray at path '%s' (%s)\n",
+                args.ndarray_path,
+                asdf_error(file));
+            goto cleanup;
+        }
+
+        if (args.raw)
+            data = asdf_ndarray_data_raw(ndarray, &size);
+        else
+            data = asdf_ndarray_data(ndarray, &size);
+    }
+
+    if (!data) {
+        const char *err = asdf_error(file);
+        fprintf(stderr, "error: block could not be opened (%s)\n", err ? err : "unknown");
+        goto cleanup;
+    }
+
+    if (strcmp(args.output, "-") == 0)
+        fp = stdout;
+    else if (args.output) {
+        errno = 0;
+        fp = fopen(args.output, "wb");
+
+        if (!fp) {
+            fprintf(
+                stderr,
+                "error: failed to open output file '%s': %s\n",
+                args.output,
+                strerror(errno));
+            return EXIT_FAILURE;
+        }
+
+        close = true;
+    }
+
+    size_t chunk_size = args.chunk_size ? args.chunk_size : size;
+
+    while (written < size) {
+        size_t remaining = size - written;
+        size_t chunk = remaining < chunk_size ? remaining : chunk_size;
+        size_t n = fwrite((const uint8_t *)data + written, 1, chunk, fp);
+
+        if (n < args.chunk_size) {
+            if (ferror(fp)) {
+                fprintf(stderr, "error: failed writing output: %s\n", strerror(errno));
+                goto cleanup;
+            }
+            break;
+        }
+
+        written += n;
+    }
+
+    status = EXIT_SUCCESS;
+cleanup:
+    asdf_block_close(block);
+    asdf_ndarray_destroy(ndarray);
+    asdf_close(file);
+
+    if (fp && close)
+        fclose(fp);
+
+    return status;
+}
+
+
+static int dd_main(int argc, char **argv) {
+    struct dd_args args = {.chunk_size = 0};
+    argp_parse(&dd_argp, argc, argv, ARGP_NO_HELP, NULL, &args);
+    return dd_run(args);
+}
+/* end dd subcommand */
+
+
+/* info subcommand */
 static char info_doc[] = "Print a rendering of an ASDF tree";
 static char info_args_doc[] = "FILENAME";
 
@@ -156,10 +381,10 @@ static int info_main(int argc, char **argv) {
     argp_parse(&info_argp, argc, argv, ARGP_NO_HELP, NULL, &args);
     return info_run(args);
 }
-// end info
+/* end info subcommand */
 
 
-// events subcommand
+/* events subcommand */
 static char events_doc[] = "Print event stream from ASDF parser (for debugging)";
 static char events_args_doc[] = "FILENAME";
 
@@ -278,10 +503,10 @@ static int events_main(int argc, char **argv) {
     argp_parse(&events_argp, argc, argv, ARGP_NO_HELP, NULL, &args);
     return events_run(args);
 }
-// end events
+/* end events subcommand */
 
 
-// verify-checksums sub-command
+/* verify-checksums sub-command */
 static char verify_checksums_doc[] = "Verify binary block MD5 checksums";
 static char verify_checksums_args_doc[] = "FILENAME";
 
@@ -411,7 +636,7 @@ static int verify_checksums_main(int argc, char **argv) {
     argp_parse(&verify_checksums_argp, argc, argv, ARGP_NO_HELP, NULL, &args);
     return verify_checksums_run(args);
 }
-// end verify-checksums
+/* end verify-checksums subcommand */
 
 
 /* Table of sub-commands, indexed by asdf_subcmd_t.  ASDF_SUBCMD_NONE (0) is a
@@ -420,6 +645,7 @@ static int verify_checksums_main(int argc, char **argv) {
 // clang-format off
 static const asdf_subcmd_def_t subcmds[] = {
     [ASDF_SUBCMD_NONE]             = {0},
+    [ASDF_SUBCMD_DD]               = {"dd", dd_doc, dd_main},
     [ASDF_SUBCMD_INFO]             = {"info", info_doc, info_main},
     [ASDF_SUBCMD_EVENTS]           = {"events", events_doc, events_main},
     [ASDF_SUBCMD_VERIFY_CHECKSUMS] = {"verify-checksums", verify_checksums_doc, verify_checksums_main},
