5266cb420e80c438947d81c36a22ed830711486c
E. Madison Bray <676149+embray@users.noreply.github.com>
2026-07-10 01:14:12 +0200

    fix: superfluous log warning deserializing the software tag
    The author and homepage properties are optional; it's fine if they're
    missing--the warning log only applies if it's the wrong type (not a
    string)

--- src/core/software.c
+++ src/core/software.c
@@ -104,14 +104,14 @@ static asdf_value_err_t asdf_software_deserialize(
     asdf_value_destroy(prop);
 
     prop = asdf_mapping_get(software_map, "homepage");
-    if (ASDF_VALUE_OK != asdf_value_as_string0(prop, &homepage))
+    if (prop && ASDF_VALUE_OK != asdf_value_as_string0(prop, &homepage))
         ASDF_LOG(
             value->file, ASDF_LOG_WARN, "ignoring invalid value for for homepage in software tag");
 
     asdf_value_destroy(prop);
 
     prop = asdf_mapping_get(software_map, "author");
-    if (ASDF_VALUE_OK != asdf_value_as_string0(prop, &author))
+    if (prop && ASDF_VALUE_OK != asdf_value_as_string0(prop, &author))
         ASDF_LOG(
             value->file, ASDF_LOG_WARN, "ignoring invalid value for for author in software tag");
 
